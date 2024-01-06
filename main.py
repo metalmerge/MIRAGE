fee88e24314ad2c506c1a14761d478c6b1cd3b5e
Dimitry Ermakov <ermakovd06@gmail.com>
2024-01-06 00:11:23 -0500

    Update Twitter API authentication and tweet with media

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
 import tweepy
 import configparser
 import sys
-import os
-
-
-def api():
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-    try:
-        TWITTER_ACCESS_TOKEN = config.get("Credentials", "TWITTER_ACCESS_TOKEN")
-        # API_key = config.get("Credentials", "API_key")
-        # API_Key_Secret = config.get("Credentials", "API_Key_Secret")
-        TWITTER_ACCESS_TOKEN_SECRET = config.get(
-            "Credentials", "TWITTER_ACCESS_TOKEN_SECRET"
-        )
-        # BEARER_TOKEN = config.get("Credentials", "BEARER_TOKEN")
-        TWITTER_CONSUMER_KEY = config.get("Credentials", "API_key")
-        TWITTER_CONSUMER_SECRET = config.get("Credentials", "API_Key_Secret")
-    except configparser.Error:
-        print(configparser.Error)
-        sys.exit(1)
-    auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
-    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-    print("Authenticated")
-    return tweepy.API(auth)
-
-
-def tweet(api: tweepy.API, message: str, image_path=None):
-    if image_path:
-        api.update_status_with_media(filename=image_path, status=message)
-    else:
-        api.update_status(status=message)
-    print(f"Tweeted: {message}")
 
 
 if __name__ == "__main__":
-    api = api()
-    tweet(api, "Finally got ahold of a Windows computer", "meme.png")
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    consumer_secret = config.get("Credentials", "consumer_secret")
+    consumer_key = config.get("Credentials", "consumer_key")
+    access_token = config.get("Credentials", "access_token")
+    access_token_secret = config.get("Credentials", "access_token_secret")
+    client = tweepy.Client(
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
+    )
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    media = api.media_upload("meme.jpg")
+
+    # Get the media ID from the uploaded media
+    media_id = media.media_id
+    client.create_tweet(
+        text="",
+        media_ids=[media_id],
+    )
+# TODO: add folder, automate web scrapping, make it work for jpg or png
