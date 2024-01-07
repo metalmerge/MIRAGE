f0b0d56b1d325c755074bf7c66d9ee424f9f527c
Dimitry Ermakov <ermakovd06@gmail.com>
2024-01-07 16:56:51 -0500

    Refactored code

--- main.py
+++ main.py
@@ -3,36 +3,54 @@ import configparser
 import os
 import random
 
-if __name__ == "__main__":
+config = configparser.ConfigParser()
+config.read(r"C:\Users\ermak\OneDrive\Documents\MIRAGE\config.ini")
+consumer_secret = config.get("Credentials", "consumer_secret")
+consumer_key = config.get("Credentials", "consumer_key")
+access_token = config.get("Credentials", "access_token")
+access_token_secret = config.get("Credentials", "access_token_secret")
+
+
+def authenticate_twitter():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    return api
+
+
+def upload_tweet_with_media(api, media_path):
+    client = tweepy.Client(
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
+    )
+    text = ""
+    media = api.media_upload(media_path)
+    media_id = media.media_id
+    client.create_tweet(
+        text=text,
+        media_ids=[media_id],
+    )
+
+
+def main():
     try:
-        config = configparser.ConfigParser()
-        config.read(r"C:\Users\ermak\OneDrive\Documents\MIRAGE\config.ini")
-        consumer_secret = config.get("Credentials", "consumer_secret")
-        consumer_key = config.get("Credentials", "consumer_key")
-        access_token = config.get("Credentials", "access_token")
-        access_token_secret = config.get("Credentials", "access_token_secret")
-        client = tweepy.Client(
-            consumer_key=consumer_key,
-            consumer_secret=consumer_secret,
-            access_token=access_token,
-            access_token_secret=access_token_secret,
-        )
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
-        api = tweepy.API(auth)
+        api = authenticate_twitter()
+
         meme_folder = r"C:\Users\ermak\OneDrive\Documents\MIRAGE\memeFolder"
         meme_files = os.listdir(meme_folder)
         media_path = os.path.join(meme_folder, random.choice(meme_files))
-        tweet_text = ""
-        media = api.media_upload(media_path)
-        text = ""
-        media_id = media.media_id
-        client.create_tweet(
-            text=text,
-            media_ids=[media_id],
-        )
+
+        upload_tweet_with_media(api, media_path)
+
         os.remove(media_path)
     except Exception as e:
         with open("error.txt", "w") as file:
             file.write(str(e))
-# TODO: automate web scrapping,
+
+
+# TODO: automate web scraping
+if __name__ == "__main__":
+    main()
