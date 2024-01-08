da91641ae94f3d3676e448a9e93c8d1047341b07
Dimitry Ermakov <ermakovd06@gmail.com>
2024-01-07 20:56:23 -0500

    added amount of images in memefolder and add progress bar

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tweepy
 import configparser
 import os
 import random
+from SubDownload import reddit_image_grabber
 
 config = configparser.ConfigParser()
 config.read(r"C:\Users\ermak\OneDrive\Documents\MIRAGE\config.ini")
@@ -9,6 +10,13 @@ consumer_secret = config.get("Credentials", "consumer_secret")
 consumer_key = config.get("Credentials", "consumer_key")
 access_token = config.get("Credentials", "access_token")
 access_token_secret = config.get("Credentials", "access_token_secret")
+sender_email = config.get("Credentials", "sender_email")
+password = config.get("Credentials", "password")
+receiver_email = config.get("Credentials", "receiver_email")
+body = "Go through the scraped meme images and then transfer the ones you like to the memeFolder."
+subject = "Alert: There are less than 3 images in the memeFolder for the Twitter Bot"
+# TODO: make a program to go through images and transfer them to the memeFolder
+# TODO: fix email sending
 
 
 def authenticate_twitter():
@@ -37,10 +45,29 @@ def upload_tweet_with_media(api, media_path):
 
 def main():
     try:
+        reddit_image_grabber("week", 10)
         api = authenticate_twitter()
 
         meme_folder = r"C:\Users\ermak\OneDrive\Documents\MIRAGE\memeFolder"
-        meme_files = os.listdir(meme_folder)
+        meme_files = [
+            f
+            for f in os.listdir(meme_folder)
+            if f.endswith(
+                (".png", ".jpg", ".jpeg", ".webp", ".PNG", ".JPG", ".JPEG", ".WEBP")
+            )
+        ]
+
+        print(f"There are {len(meme_files)-1} images in the memeFolder.")
+
+        if len(meme_files) < 3:
+            print("Alert: There are less than 3 images in the memeFolder.")
+            # import yagmail
+            # try:
+            #     yag = yagmail.SMTP(sender_email, password)
+            #     yag.send(receiver_email, subject, body)
+            # except Exception as e:
+            #     print(f"An error occurred: {e}")
+
         media_path = os.path.join(meme_folder, random.choice(meme_files))
 
         upload_tweet_with_media(api, media_path)
@@ -51,6 +78,5 @@ def main():
             file.write(str(e))
 
 
-# TODO: automate web scraping
 if __name__ == "__main__":
     main()
